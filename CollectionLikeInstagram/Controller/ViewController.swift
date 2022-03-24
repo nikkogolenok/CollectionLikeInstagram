7d0f38d1a99fd14b3de78ebd58ac2f6d913eddfd
nikita_kogolenok <nikiton17ns@gmail.com>
2022-03-24 10:15:40 +0300

    Initial Commit

---
+++ CollectionLikeInstagram/Controller/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  CollectionLikeInstagram
+//
+//  Created by Никита Коголенок on 24.03.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    // MARK: - View Life Cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
