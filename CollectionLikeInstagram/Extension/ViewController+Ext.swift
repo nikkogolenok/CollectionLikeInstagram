47e2aaeca2ab5d97e5a6b48f78f5bf0ef5ce5b98
nikita_kogolenok <nikiton17ns@gmail.com>
2022-03-24 11:12:07 +0300

    Add extension with standart func for collecion

---
+++ CollectionLikeInstagram/Extension/ViewController+Ext.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController+Ext.swift
+//  CollectionLikeInstagram
+//
+//  Created by Никита Коголенок on 24.03.22.
+//
+
+import UIKit
+
+extension ViewController: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        return UICollectionViewCell()
+    }
+    
+    
+}
